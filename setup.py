663a648891d6a4ae3d12da237bb79db8ca6748a4
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-05-11 18:05:35 +0200

    setup.py: fixes du to changes in python

--- setup.py
+++ setup.py
@@ -20,19 +20,19 @@
 # along with damealgorithms; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
-
+import os
 from setuptools import setup
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
+
 setup(name='damealgorithms',
-      version='0.0.9',
+      version='0.0.11',
       description='A library to store simple algorithms',
       long_description=long_description,
-      long_description_content_type='text/markdown',
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
