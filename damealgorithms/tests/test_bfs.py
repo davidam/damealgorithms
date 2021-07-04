eeb21dc8cb0f71ffa55b3303a9bd1726f5e333f7
David Arroyo Menéndez <davidam@gmail.com>
2021-07-04 17:12:30 +0200

    tests/test_bfs.py, src/bfs.py: fixing PEP8

--- damealgorithms/tests/test_bfs.py
+++ damealgorithms/tests/test_bfs.py
@@ -24,6 +24,7 @@
 from unittest import TestCase
 from src.bfs import Graph
 
+
 class TestBfs(TestCase):
 
     def test_bfs_create(self):
@@ -33,21 +34,5 @@ class TestBfs(TestCase):
         g.addEdge(1, 2)
         g.addEdge(2, 0)
         g.addEdge(2, 3)
-        g.addEdge(3, 3)        
+        g.addEdge(3, 3)
         self.assertEqual("2 0 3 1 ", g.BFS(2))
-
-    # def test_bfs_helper(self):
-    #     alist = [54,26,93,17,77,31,44,55,20]
-    #     q = Quicksort(alist)
-    #     self.assertEqual(None, q.quickSortHelper(alist, 54, 20))
-        
-    # def test_quicksort_partition(self):
-    #     l = [54,26,93,17,77,31,44,55,20]
-    #     q = Quicksort(l)
-    #     self.assertEqual([54, 26], q.partition(l, 0, 1))
-
-
-    # def test_quicksort_quicksort(self):
-    #     alist = [54,26,93,17,77,31,44,55,20]
-    #     q = Quicksort(alist)
-    #     self.assertEqual([54,26,93,17,77,31,44,55,20], q.quickSort(alist))
